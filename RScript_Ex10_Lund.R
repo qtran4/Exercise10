1a2b4a32d483d71593ef90871ba91f724739f4d4
avivalund <avivalund@gmail.com>
2022-11-30 14:24:53 -0500

    done

--- RScript_Ex10_Lund.R
+++ RScript_Ex10_Lund.R
@@ -1,3 +1,5 @@
+##Question 1
+
 # Set working directory
 setwd("/Users/avivalund/Desktop/Biocomputing/Exercise10")
 
@@ -6,42 +8,55 @@ GoBadgers<-read.table("UWvMSU_1-22-13.txt", header=TRUE)
 
 # make a dataframe with a cumulative score 
 # for each team whenever either team scores
-  #isolate one team at a time
 
+#UW
 cumUW = c(0)
+cumMSU = c(0)
 for(file in 1:nrow(GoBadgers)){
   one_row_data = GoBadgers[file,]
-  if(one_row_data$team[file] == "UW"){
+  if(one_row_data$team == "UW"){
     currentcumUW = cumUW[file] + one_row_data$score
+    currentcumMSU = cumMSU[file] + 0
   }else{
+    currentcumMSU = cumMSU[file] + one_row_data$score
     currentcumUW = cumUW[file] + 0
   }
   cumUW = c(cumUW, currentcumUW)
+  cumMSU = c(cumMSU, currentcumMSU)
     
 }
-grep("UW", team) # how do I isolate a team?
-df$csum <- ave(time, team, score=cumsum)
-#OR
-UWisconsin<- data.frame(time, team, score)
-
-  #cumulative score thing -how do i properly do this?
-
-#isolate one team at a time
-grep("MSU", team) # how do I isolate a team?
-df$csum <- ave(time, team, score=cumsum)
-#OR
-MSU<- data.frame(time, team, score)
-  #cumulative score thing -how do i properly do this?
 
 # load a package every time you wish to use it
 library(ggplot2)
 library(cowplot)
 
+#create a dataframe with adjusted time
+basketball <-data.frame(time = c(0, GoBadgers$time), cumUW, cumMSU)
 
 #Make plot without grey background and gridlines
 
-ggplot(data = GoBadgers,
-       aes(x = time, y = cumscore)) +
-  geom_point() +
-  stat_smooth(method="loess") +
+ggplot(data = basketball,
+       aes(x = time)) +
+  geom_line(aes(y=cumUW), color="blue") +
+  geom_line(aes(y=cumMSU), color="pink") +
   theme_classic()
+
+
+## Question 2
+randomnumber<-sample(1:100, 1)
+
+for(file in 1:11){
+  if(file==11){
+    print("GAME OVER!")
+    break
+  }
+  input=readline(prompt="Guess:")
+  if(input<randomnumber){
+    print("this is lower than actual value")
+  } else if(input>randomnumber){
+    print("this is higher than actual value")
+  } else {
+    print("Correct!")
+    break
+  }
+}
